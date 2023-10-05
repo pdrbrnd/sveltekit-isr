226f7e5532b6cf54eb78eddea5e184fd11761e65
Pedro Brandão <pedro@significa.co>
2023-10-05 21:56:31 +0100

    change menu order

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -5,11 +5,11 @@
 </ul>
 
 <ul>
-  <li><a href="/working">/working (ISR) ✅</a></li>
-  <li><a href="/working/foo">/working/[...path] (ISR) ✅</a></li>
+  <li><a href="/not-working">/not-working (ISR) ❌</a></li>
+  <li><a href="/not-working/foo">/not-working/[...path] (ISR) ✅</a></li>
 </ul>
 
 <ul>
-  <li><a href="/not-working">/not-working (ISR) ❌</a></li>
-  <li><a href="/not-working/foo">/not-working/[...path] (ISR) ✅</a></li>
+  <li><a href="/working">/working (ISR) ✅</a></li>
+  <li><a href="/working/foo">/working/[...path] (ISR) ✅</a></li>
 </ul>
