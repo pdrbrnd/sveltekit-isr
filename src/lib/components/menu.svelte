a78faa665f8534df7d8d98543a9a1be7b32ccd02
Pedro Brandão <pedro@significa.co>
2023-10-05 21:34:02 +0100

    remove nesting

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,8 +1,7 @@
 <p>Navigate and refresh the page:</p>
 
 <ul>
-  <li><a href="/">/</a> &mdash; ✅ (no ISR)</li>
-  <li><a href="/us">/[region]</a> &mdash; ✅ (no ISR)</li>
-  <li><a href="/us/en">/[region]/[lang]</a> &mdash; ❌ (ISR)</li>
-  <li><a href="/us/en/foo">/[region]/[lang]/[...path]</a> &mdash; ✅ (ISR)</li>
+  <li><a href="/">/</a></li>
+  <li><a href="/en">/[lang]</a></li>
+  <li><a href="/en/foo">/[lang]/[...path]</a></li>
 </ul>
