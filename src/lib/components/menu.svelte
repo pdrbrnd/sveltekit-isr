03ddd3cde2655f06dd29cde3fcff85712b4c409d
Pedro Brandão <pedro@significa.co>
2023-10-05 21:46:38 +0100

    remove path params

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,7 +1,15 @@
 <p>Navigate and refresh the page:</p>
 
 <ul>
-  <li><a href="/">/</a></li>
-  <li><a href="/en">/[lang]</a></li>
-  <li><a href="/en/foo">/[lang]/[...path]</a></li>
+  <li><a href="/">/ (no ISR)</a></li>
+</ul>
+
+<ul>
+  <li><a href="/working">/working (ISR) ✅</a></li>
+  <li><a href="/working/foo">/working/[...path] (ISR) ✅</a></li>
+</ul>
+
+<ul>
+  <li><a href="/not-working">/not-working (ISR) ❌</a></li>
+  <li><a href="/not-working/foo">/not-working/[...path] (ISR) ✅</a></li>
 </ul>
---
+++ src/routes/not-working/[...path]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Menu from '$lib/components/menu.svelte';
+  import Params from '$lib/components/params.svelte';
+  import Revalidate from '$lib/components/revalidate.svelte';
+
+  export let data;
+</script>
+
+<Menu />
+<Params />
+
+<p>Random number: {data.number}</p>
+
+<Revalidate />
