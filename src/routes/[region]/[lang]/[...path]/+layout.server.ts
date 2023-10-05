9a79813b19db06ba2c15bbe043e910d50a063e7e
Pedro Brandão <pedro@significa.co>
2023-10-05 20:44:32 +0100

    layout server load fn

--- src/routes/[region]/[lang]/[...path]/+layout.server.ts
+++ src/routes/[region]/[lang]/[...path]/+layout.server.ts
@@ -1,8 +1,14 @@
 import { ISR_BYPASS_TOKEN } from '$env/static/private';
 
+export const load = () => {
+  return {
+    number: Math.random()
+  };
+};
+
 export const config = {
   isr: {
     bypassToken: ISR_BYPASS_TOKEN,
-    expiration: 60
+    expiration: 60 * 10 // 10 minutes
   }
 };
--- src/routes/[region]/[lang]/[...path]/+page.svelte
+++ src/routes/[region]/[lang]/[...path]/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import Menu from '$lib/components/menu.svelte';
   import Params from '$lib/components/params.svelte';
+
+  export let data;
 </script>
 
 <Menu />
 <Params />
 
-<p>Random number: {Math.random()}</p>
+<p>Random number: {data.number}</p>
