de2a2382b9e7065041ecda0dbcce32df62c57bd7
Pedro Brandão <pedro@significa.co>
2023-10-05 21:08:44 +0100

    add revalidate button

--- src/routes/[region]/[lang]/[...path]/+page.svelte
+++ src/routes/[region]/[lang]/[...path]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Menu from '$lib/components/menu.svelte';
   import Params from '$lib/components/params.svelte';
+  import Revalidate from '$lib/components/revalidate.svelte';
 
   export let data;
 </script>
@@ -9,3 +10,5 @@
 <Params />
 
 <p>Random number: {data.number}</p>
+
+<Revalidate />
