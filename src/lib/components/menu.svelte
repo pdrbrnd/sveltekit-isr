f32d3d1d5db5d44a31639cac4379fa9e6d0ec03c
Pedro Brandão <pedro@significa.co>
2023-10-05 21:20:38 +0100

    add page

--- src/lib/components/menu.svelte
+++ src/lib/components/menu.svelte
@@ -1,6 +1,8 @@
+<p>Navigate and refresh the page:</p>
+
 <ul>
-  <li><a href="/">/</a></li>
-  <li><a href="/us">/[region]</a></li>
-  <li><a href="/us/en">/[region]/[lang]</a></li>
-  <li><a href="/us/en/foo">/[region]/[lang]/[...path]</a></li>
+  <li><a href="/">/</a> &mdash; ✅ (no ISR)</li>
+  <li><a href="/us">/[region]</a> &mdash; ✅ (no ISR)</li>
+  <li><a href="/us/en">/[region]/[lang]</a> &mdash; ❌ (ISR)</li>
+  <li><a href="/us/en/foo">/[region]/[lang]/[...path]</a> &mdash; ✅ (ISR)</li>
 </ul>
---
+++ src/routes/[region]/[lang]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Menu from '$lib/components/menu.svelte';
+  import Params from '$lib/components/params.svelte';
+</script>
+
+<Menu />
+<Params />
